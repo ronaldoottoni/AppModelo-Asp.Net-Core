28592a2b2c44747dde9fbc6bacb125cdbfc0f4db
Ronaldo Ottoni Borges de Carvalho <ronaldoottoni@gmail.com>
2020-10-20 21:25:50 -0300

    Home View Index
    Criacação da primeira View (Index)

--- src/Express.UI.ERP/Controllers/HomeController.cs
+++ src/Express.UI.ERP/Controllers/HomeController.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Express.UI.ERP.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-
+        //Faz com que seja retornada a view padrao ligada a controller, no caso a Index
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
