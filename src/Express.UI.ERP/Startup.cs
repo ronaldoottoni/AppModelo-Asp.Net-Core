2360c4f93c36cafdb0190c0e3087843f73ab7d1c
Ronaldo Ottoni Borges de Carvalho <ronaldoottoni@gmail.com>
2020-10-20 21:20:24 -0300

    Configurando MVC
    Passos de configuração do ambiente para se comportar como MVC.
    Criação do arquivo appsetting.json.
    Criação da HomeController

--- src/Express.UI.ERP/Startup.cs
+++ src/Express.UI.ERP/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Express.UI.ERP
@@ -15,6 +16,7 @@ namespace Express.UI.ERP
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +27,21 @@ namespace Express.UI.ERP
                 app.UseDeveloperExceptionPage();
             }
 
+
+            app.UseMvc( routes => 
+            {
+                //as tags name e template nao sao obrigatorias, pode ser especificado os atributos sem elas
+                routes.MapRoute(
+                    name: "default", 
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            /*
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });
+            */
         }
     }
 }
